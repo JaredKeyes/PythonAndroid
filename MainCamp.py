e106a50c68210c4edf19e146c668092e4d3ca8db
Jared Keyes <Jared.Keyes3@gmail.com>
2024-11-08 11:30:19 -0500

    Updated maincamp.py with more option

--- MainCamp.py
+++ MainCamp.py
@@ -1,12 +1,19 @@
 import Fork
 
 def main_camp():
-	print("You are at your main camp.")
-	choice = input("Would you like to go to the fork? Yes or No?\n").lower()
+	print("You are at your main camp.\n")
+	print("To the NORTH, there is a dirt road.\nTo the EAST is a dense forest.\nLooking SOUTH, you see a vast ocean.\nOff to the WEST is a large mountain range.\nYou can also stay at the camp and REST.\n") 
+	choice = input("Which do you choose?\n").lower()
 	
-	if choice == "yes":
+	if choice == "north":
 		Fork.fork()
-	elif choice == "no":
+	elif choice == "east":
+		print("You head to the forest.")
+	elif choice == "south":
+		print("You go to the ocean.")
+	elif choice == "west":
+		print("You travel to the mountains.")
+	elif choice == "rest":
 		print("You rest for the day.")
 	else:
 		print("That's not a valid choice.")
